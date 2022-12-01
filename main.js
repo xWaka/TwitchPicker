9a6b91d94702a02f97c38245b76e55583011d30e
Rob Lee <coachroblee@gmail.com>
2022-12-01 13:49:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+document.getElementById("purple").onclick = partyPurple;
+document.getElementById("green").onclick = partyGreen;
+document.getElementById("blue").onclick = partyBlue;
+document.getElementById("red").onclick = partyRed;
+document.getElementById("yellow").onclick = partyYellow;
+
+function partyPurple() {
+  document.querySelector("body").style.backgroundColor = "rgba(241,63,247,1)";
+  document.querySelector("body").style.color = "white";
+}
+
+function partyGreen() {
+  document.querySelector("body").style.backgroundColor = "rgba(0,253,81,1)";
+  document.querySelector("body").style.color = "white";
+}
+
+function partyBlue() {
+  document.querySelector("body").style.backgroundColor = "rgba(0,254,255)";
+  document.querySelector("body").style.color = "white";
+}
+
+function partyRed() {
+  document.querySelector("body").style.backgroundColor = "rgb(238, 9, 9)";
+  document.querySelector("body").style.color = "white";
+}
+
+
+function partyYellow(){
+  document.querySelector("body").style.backgroundColor ="yellow"
+  document.querySelector("body").style.color= "white"
+}
